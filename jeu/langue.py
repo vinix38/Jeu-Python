65ff9654943ad8901d056cd603864db0e6d9259d
vinix38 <vinceguichard04@gmail.com>
2021-02-25 19:04:51 +0100

    init loc

---
+++ jeu/langue.py
@@ -0,0 +1,13 @@
+#====== FRANCAIS ======
+fr_FR = {
+    "titre1" : "je suis un titre",
+    "dialogue1" : "je suis un dialogue",
+    "debut" : "bienvenue!",
+}
+
+#====== ANGLAIS ======
+en_US = {
+    "titre1" : "I'm a title",
+    "dialogue1" : "I'm a dialog",
+    "debut" : "welcome!",
+}
